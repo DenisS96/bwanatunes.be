1493652f669fbeb364a87ff71c8a2ad014e601ea
DenisS96 <88380857+DenisS96@users.noreply.github.com>
2022-03-21 09:43:35 +0100

    Add files via upload

--- script/script2.js
+++ script/script2.js
@@ -2,7 +2,7 @@ var post = document.getElementById("post");
 post.addEventListener("click", function () {
     var nameBoxValue = document.getElementById("name-box").value;
 
-    var li = document.createElement("p");
+    var li = document.createElement("li");
     var text = document.createTextNode(nameBoxValue);
     li.appendChild(text);
     document.getElementById("unordered").appendChild(li);
@@ -14,7 +14,7 @@ post.addEventListener("click", function () {
     var commentBoxValue = document.getElementById("comment-box").value;
 
     var li = document.createElement("p");
-    var text = document.createTextNode(commentBoxValue);
+    var text = document.createTextNode("- " + commentBoxValue);
     li.appendChild(text);
     document.getElementById("unordered").appendChild(li);
 
